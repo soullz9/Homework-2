f2472d8d0b407b00721b4c7772947690139105b8
Maxim <lollollol3328@gmail.com>
2022-11-22 17:53:56 +0300

    homework

---
+++ Задача10/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a)
+{
+
+    int result = ((a / 10) % 10);
+    return result;
+}
